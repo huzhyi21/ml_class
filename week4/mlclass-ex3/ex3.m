6f156d8a0a99da0461e3da22bb51f5bb1718ae97
Jan Brusch <janbrusch@Euklid.local>
2015-02-24 13:50:50 +0100

    completed first excercise

--- week4/mlclass-ex3/ex3.m
+++ week4/mlclass-ex3/ex3.m
@@ -18,6 +18,11 @@
 
 %% Initialization
 clear ; close all; clc
+% set working directory
+% mac:
+chdir /Users/janbrusch/Documents/coursera/ml_class/week4/mlclass-ex3
+% win:
+% chdir C:/Users/Jan Brusch/Documents/Coding/Octave/ml_class/week3/mlclass-ex2
 
 %% Setup the parameters you will use for this part of the exercise
 input_layer_size  = 400;  % 20x20 Input Images of Digits
--- week4/mlclass-ex3/lrCostFunction.m
+++ week4/mlclass-ex3/lrCostFunction.m
@@ -37,12 +37,17 @@ grad = zeros(size(theta));
 %
 
 
+% h0x denotes Hyopthesis of X
+h0x = sigmoid(X*theta);
+% theta without a normalizer for Theta0
+theta_reg = theta(2:length(theta));
 
+%cost function
+J = (1/m) * sum((-y'*log(h0x) - (1-y')*log(1-h0x))) + (lambda/(m*2)) * (theta_reg'*theta_reg);
 
-
-
-
-
+% grad function
+grad = ((1./m) * (X'*(h0x - y)));
+grad(2:length(grad)) = grad(2:length(grad)) + (lambda/m)*theta_reg;
 
 
 % =============================================================
